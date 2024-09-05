e8059e9cbf178327a002318407dbfc05f91104e2
kbilinski <Kaje.bile@gmail.com>
2024-09-05 19:50:47 +0200

    Walking Skeleton Client

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175c8bcd1bddda9315a7f90028807fbfc990f807")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c7806122c35555e61e8823041d640caee8e5d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
