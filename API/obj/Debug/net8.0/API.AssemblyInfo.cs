89d606b7264077d0cad03b37fb88562ecbddf6de
kbilinski <Kaje.bile@gmail.com>
2024-09-09 16:49:26 +0200

    Adding Form and Bar
    Added Login Form and Navigation bar

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d66609d8ef474632386c516905ac9da31e778f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d51813b499d850a9eec600adf2e015910a0dd4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
