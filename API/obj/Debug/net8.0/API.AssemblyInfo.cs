39928b8a3b5e9b849bf02a382f89e4a560dd5b71
kbilinski <Kaje.bile@gmail.com>
2024-10-21 21:51:52 +0200

    Matches
    Added member card with css styling, and member service

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb88a41e756a06ccaa5c6a82ffa0a56f43fb7dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d05ce27643e62536b26e2d015c4c52bee859e53f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
